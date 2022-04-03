b1f97e5a2fa61c1c4d65a6121f98cd75c99157ce
Adam Domaszewski <adamdox@gmail.com>
2022-04-03 22:54:30 +0200

    Make camera shake avalanche distance dependent

---
+++ Game/Assets/Scripts/DistanceShakeAware.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class DistanceShakeAware : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject referenceGameObject;
+    [SerializeField]
+    private AnimationCurve shakeLooseCurve;
+    [SerializeField]
+    private float zeroShakeDistance;
+    private CinemachineBasicMultiChannelPerlin multiChannelPerlin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        multiChannelPerlin = gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent(CinemachineCore.Stage.Noise) as CinemachineBasicMultiChannelPerlin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(transform.position, referenceGameObject.transform.position);
+        var progressToShakeLoose = Mathf.Max(1 - distance / zeroShakeDistance, 0f);
+        multiChannelPerlin.m_AmplitudeGain = shakeLooseCurve.Evaluate(progressToShakeLoose);
+        Debug.Log("distance=" + distance + ", shake=" + progressToShakeLoose);
+    }
+}
