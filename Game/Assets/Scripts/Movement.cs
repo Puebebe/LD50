deed75afbbdb63149382b981d12435857bd98d00
Adam Domaszewski <adamdox@gmail.com>
2022-04-03 18:44:51 +0200

    Add avalanche constant movement

---
+++ Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+public class Movement : MonoBehaviour
+{
+    public Vector3 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += speed;
+    }
+}
