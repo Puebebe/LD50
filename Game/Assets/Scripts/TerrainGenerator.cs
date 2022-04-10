799b83ae1d827e227605efa5cd47c083e9b344b7
Adam Domaszewski <adamdox@gmail.com>
2022-04-10 16:11:36 +0200

    Add generating side walls

--- Game/Assets/Scripts/TerrainGenerator.cs
+++ Game/Assets/Scripts/TerrainGenerator.cs
@@ -24,6 +24,10 @@ public class TerrainGenerator : MonoBehaviour
     private float deformationMaxHeight = 2;
     [SerializeField, Range(0f, 1f)]
     private float deformationPropability = 0.5f;
+    [SerializeField]
+    private int sideDeformationWidth = 5;
+    [SerializeField]
+    private float sideDeformationHeight = 50f;
 
 
     private MeshCollider meshCollider;
@@ -38,9 +42,10 @@ public class TerrainGenerator : MonoBehaviour
         meshCollider = GetComponent<MeshCollider>();
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        vertices = new Vector3[(xSize + 1) * (2 * drawDistance + 1)];
+        vertices = new Vector3[(xSize + 1 + 2 * sideDeformationWidth) * (2 * drawDistance + 1)];
 
         GenerateVertices(0);
+        GenerateSideDeformation(0);
         UpdateLastLocation();
         CreateTriangles();
         UpdateMesh();
@@ -49,11 +54,11 @@ public class TerrainGenerator : MonoBehaviour
     private void GenerateVertices(int fromRow)
     {
         var positionZ = (int)center.position.z;
-        var xSizeHalf = xSize / 2;
-        var startZ = fromRow * (xSize + 1);
-        for (int i = startZ, z = positionZ - drawDistance + fromRow; z <= positionZ + drawDistance; z++)
+        var widthHalf = xSize / 2 + sideDeformationWidth;
+        var startIndex = fromRow * (xSize + 1 + 2 * sideDeformationWidth);
+        for (int i = startIndex, z = positionZ - drawDistance + fromRow; z <= positionZ + drawDistance; z++)
         {
-            for (int x = -xSizeHalf; x <= xSizeHalf; x++)
+            for (int x = -widthHalf; x <= widthHalf; x++)
             {
                 float y = Mathf.PerlinNoise(x * scale, z * scale);
                 vertices[i] = new Vector3(x, y, z);
@@ -69,19 +74,20 @@ public class TerrainGenerator : MonoBehaviour
 
     private void CreateTriangles()
     {
-        triangles = new int[xSize * 2 * drawDistance * 6];
+        triangles = new int[(xSize + sideDeformationWidth * 2) * 2 * drawDistance * 2 * 3];
         int vert = 0;
         int tris = 0;
+        var totalWidth = xSize + 2 * sideDeformationWidth;
         for (int z = -drawDistance; z < drawDistance; z++)
         {
-            for (int x = 0; x < xSize; x++)
+            for (int x = 0; x < totalWidth; x++)
             {
                 triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 1] = vert + totalWidth + 1;
                 triangles[tris + 2] = vert + 1;
                 triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + xSize + 1;
-                triangles[tris + 5] = vert + xSize + 2;
+                triangles[tris + 4] = vert + totalWidth + 1;
+                triangles[tris + 5] = vert + totalWidth + 2;
 
                 vert++;
                 tris += 6;
@@ -116,6 +122,7 @@ public class TerrainGenerator : MonoBehaviour
     {
         ShiftVerticesBy(shiftZ);
         GenerateVertices(2 * drawDistance - shiftZ);
+        GenerateSideDeformation(2 * drawDistance - shiftZ);
         if (Random.Range(0f, 1f) <= deformationPropability)
         {
             GenerateDeformation(2 * drawDistance - shiftZ);
@@ -123,19 +130,59 @@ public class TerrainGenerator : MonoBehaviour
         UpdateLastLocation();
     }
 
+    private void ShiftVerticesBy(int shift)
+    {
+        for (int i = 0, j = shift * (xSize + 1 + 2 * sideDeformationWidth); j < vertices.Length; j++, i++)
+        {
+            vertices[i] = vertices[j];
+        }
+    }
+
+    private void GenerateSideDeformation(int fromRow)
+    {
+        var positionZ = (int)center.position.z;
+        var xSizeHalf = xSize / 2;
+        var startIndex = fromRow * (xSize + 1 + 2 * sideDeformationWidth);
+        var deformationStartZ = positionZ - drawDistance + fromRow;
+        var deformationEndZ = positionZ + drawDistance;
+
+        for (int i = startIndex, z = deformationStartZ; z <= deformationEndZ; z++)
+        {
+            var deformationStep = Mathf.PI / (2 * sideDeformationWidth + 2);
+            var currentDeformation = 0f;
+
+            i += xSize + sideDeformationWidth;
+            for (int x = xSizeHalf; x <= xSizeHalf + sideDeformationWidth; x++)
+            {
+                vertices[i] += new Vector3(0, Mathf.Sin(currentDeformation) * sideDeformationHeight, 0);
+                i++;
+                currentDeformation += deformationStep;
+            }
+
+            i -= xSize + 2 * sideDeformationWidth + 1;
+            for (int x = -xSizeHalf - sideDeformationWidth; x <= -xSizeHalf; x++)
+            {
+                vertices[i] += new Vector3(0, Mathf.Sin(currentDeformation) * sideDeformationHeight, 0);
+                i++;
+                currentDeformation += deformationStep;
+            }
+            i += xSize + sideDeformationWidth;
+        }
+    }
+
     private void GenerateDeformation(int fromRow)
     {
         var positionZ = (int)center.position.z;
         var xSizeHalf = xSize / 2;
-        var startIndex = fromRow * (xSize + 1);
+        var startIndex = fromRow * (xSize + 1 + 2 * sideDeformationWidth);
         var deformationStartX = (int)Random.Range(transform.position.x - xSizeHalf, transform.position.x + xSizeHalf - minDeformationWidth);
         var deformationEndX = (int)Random.Range(deformationStartX + minDeformationWidth, transform.position.x + xSizeHalf);
         var deformationStartZ = (int)Random.Range(positionZ - drawDistance + fromRow, positionZ + drawDistance - minDeformationLength);
         var deformationEndZ = (int)Random.Range(deformationStartZ + minDeformationLength, positionZ + drawDistance);
         var deformationHeight = Random.Range(-deformationMaxHeight, deformationMaxHeight);
 
-        var indexPreIndent = deformationStartX - ((int)transform.position.x - xSizeHalf);
-        var indexPostIndent = ((int)transform.position.x + xSizeHalf) - deformationEndX;
+        var indexPreIndent = deformationStartX - ((int)transform.position.x - xSizeHalf) + sideDeformationWidth;
+        var indexPostIndent = ((int)transform.position.x + xSizeHalf) - deformationEndX + sideDeformationWidth;
         var deformationStepZ = Mathf.PI / (deformationEndZ - deformationStartZ);
         var currentDeformationZ = 0f;
         for (int i = startIndex + indexPreIndent, z = deformationStartZ; z <= deformationEndZ; z++)
@@ -150,14 +197,6 @@ public class TerrainGenerator : MonoBehaviour
             }
             i += indexPostIndent + indexPreIndent;
             currentDeformationZ += deformationStepZ;
-        }
-    }
-
-    private void ShiftVerticesBy(int shift)
-    {
-        for (int i = 0, j = shift * (xSize + 1); j < vertices.Length; j++, i++)
-        {
-            vertices[i] = vertices[j];
         }
     }
 
@@ -193,7 +232,7 @@ public class TerrainGenerator : MonoBehaviour
     private Vector3 GetVertexPositionAt(int x, int z)
     {
         var meshStart = transform.TransformPoint(mesh.bounds.min);
-        var vertex = vertices[(z - (int)meshStart.z) * (xSize + 1) + (x - (int)meshStart.x)];
+        var vertex = vertices[(z - (int)meshStart.z) * (xSize + 1 + 2 * sideDeformationWidth) + (x - (int)meshStart.x)];
         return transform.TransformPoint(vertex);
     }
 
